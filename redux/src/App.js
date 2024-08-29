10c32777cfb1ee10b4c18cffa202dce5daf449ee
vinoth <vinothraghu842@gmail.com>
2024-08-29 16:21:46 +0530

    update

--- redux/src/App.js
+++ redux/src/App.js
@@ -8,13 +8,15 @@ import { myStore } from './redux/config';
 import Home from './Home';
 import About from './About'; 
 import Contact from './Contact';
+import Web from './Web';
 
 function App() {
   const routerPaths = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/home", element: <Home /> },
     { path: "/About", element: <About /> },
-    { path: "/Contact", element: <Contact /> } 
+    { path: "/Contact", element: <Contact /> },
+    {path:"/web",element:<Web/>}
   ]);
 
   return (
