3fe5e9f8453fb3cbfbddffefbc3ea045683bd0fa
vinoth <vinothraghu842@gmail.com>
2024-08-29 16:21:18 +0530

    update

--- redux/src/Home.js
+++ redux/src/Home.js
@@ -1,9 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import Header from './Header';
+import {useState} from 'react';
 
 const Home = () => {
     const countVal = useSelector((state) => state.counter);
     const dispatch = useDispatch();
+    const[status,setStatus] = useState(true);
 
     const add = () => {
         dispatch({
@@ -33,6 +35,7 @@ const Home = () => {
             <input type="button" value="sub"  onClick={sub}/>
             <h4> This will display in about page! </h4>
             <button onClick = { storeDetailsFun }> display </button>
+            {(status)?<p className="red">this is paragraph</p>:null}
         </div>
     );
     
